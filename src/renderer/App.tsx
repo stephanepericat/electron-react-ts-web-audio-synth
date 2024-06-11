13341f4de968c5341459eebad88650a6a4580e5f
Stephane P. Pericat <shortwavaudio@gmail.com>
2024-06-11 09:19:45 -0500

    shadcn integration

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,12 +1,13 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
+// import icon from '../../assets/icon.svg';
+import { Button } from '../components/ui/button';
 import './App.css';
 
 function Hello() {
   return (
     <div>
-      <h1 className="bg-gray-500 text-center text-white">
-        Hi Tailwind has been integrated.
+      <h1 className="text-center p-8">
+        <Button>Hello Tailwind!</Button>
       </h1>
     </div>
   );
